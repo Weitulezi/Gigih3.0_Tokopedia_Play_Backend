98030edb2d2b601d5b2eca7be393c8177f89d67c
Dixon Vande <vandenemo@yahoo.com>
2023-08-10 03:14:30 +0700

    feat: add video controllers and routes

--- src/controllers/video.js
+++ src/controllers/video.js
@@ -1,6 +1,13 @@
 const VideoModel = require("../models/video")
 
 const createVideoController = async (req, res) => {
+    /*
+        IMPORTANT
+        Get user id, wheter from body or decoded from jwt token.
+        If jwt token use to get user id, consider using middleware,
+        or put the logic directly here.
+    */
+
     const { title, videoImage, embedId, userId } = req.body
 
     const video = new VideoModel({
@@ -17,19 +24,41 @@ const createVideoController = async (req, res) => {
             message: "Video is successfully created.",
         })
     } catch (err) {
-        res.status(400).json({ message: "Bad Requeest" })
+        res.status(400).json({
+            success: false,
+            message: "Failed to create a video.",
+        })
     }
 }
 
-const getVideoController = (req, res) => {
+const getVideoController = async (req, res) => {
     const { videoId } = req.params.videoId
 
     try {
-        const video = VideoModel.findById(videoId)
+        const video = await VideoModel.findById(videoId)
         res.status(200).json(video)
     } catch (err) {
-        res.status(400).json({})
+        res.status(400).json({
+            success: false,
+            message: "Failed to retrieve a video.",
+        })
     }
 }
 
-module.exports = { createVideoController, getVideoController }
+const getVideoListController = async (req, res) => {
+    try {
+        const videos = await VideoModel.find()
+        res.status(200).json(videos)
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: "Can't find any video.",
+        })
+    }
+}
+
+module.exports = {
+    createVideoController,
+    getVideoController,
+    getVideoListController,
+}
--- src/routes/video.js
+++ src/routes/video.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const {
     createVideoController,
     getVideoController,
+    getVideoListController,
 } = require("../controllers/video")
 
 router.post(`/videos`, createVideoController)
+router.get(`/videos`, getVideoListController)
 router.get(`/videos/:videoId`, getVideoController)

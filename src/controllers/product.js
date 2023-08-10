413f4325440635385430ac333c9e5ae0c04c6584
Dixon Vande <vandenemo@yahoo.com>
2023-08-10 16:12:30 +0700

    feat: add product controller

---
+++ src/controllers/product.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+const ProductModel = require("../models/product")
+
+const createProductController = async (req, res) => {
+    const { title, image, link, price, videoId } = req.body
+
+    const product = new ProductModel({
+        title,
+        image,
+        link,
+        price,
+        video: videoId,
+    })
+
+    try {
+        const savedProduct = await product.save()
+        res.status(200).json({
+            message: "Product is successfully created.",
+            data: savedProduct,
+        })
+    } catch (err) {
+        res.status(400).json({})
+    }
+
+    res.status(200).json()
+}
+
+export { createProductController }
